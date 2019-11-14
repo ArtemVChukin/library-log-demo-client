0353907a60aedfe321519635e48d2911e457db39
Artem Chukin <ArtemVChukin@gmail.com>
2019-11-14 23:40:56 +0300

    Minor changes

--- src/test/java/ru/alfastrah/library/log/democlient/DemoServerApplicationTests.java
+++ src/test/java/ru/alfastrah/library/log/democlient/DemoServerApplicationTests.java
@@ -3,11 +3,14 @@ package ru.alfastrah.library.log.democlient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
